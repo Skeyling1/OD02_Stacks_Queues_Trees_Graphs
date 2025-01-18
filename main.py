3d05dcecfd8a173465a342293b5337490725ed45
Skeyling1 <skeyling1@gmail.com>
2025-01-18 22:51:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,40 @@
 # last in, first out
 class Stack:
     def __init__(self):
-        self.stack = []
+        self.items = []
 
     def is_empty(self):
-        return self.stack == []
+        return self.items == []
 
-    def add_item(self, item):
-        return self.stack.append(item)
+    def push(self, item):
+        return self.items.append(item)
 
-    def take_item(self):
-        return self.stack.pop()
+    def pop(self):
+        return self.items.pop()
 
-    def upper_elmt(self):
-        return self.stack[-1]
+    def peak(self):
+        return self.items[-1]
 
 
 stack = Stack()
 
 print("список Stack пуст: ", stack.is_empty())
-stack.add_item(12)
-print(stack.upper_elmt())
-stack.add_item(13)
-print(stack.upper_elmt())
-stack.add_item(14)
-print(stack.upper_elmt())
-stack.add_item(15)
-print(stack.upper_elmt())
-stack.add_item(16)
-print(stack.upper_elmt())
+stack.push(12)
+print(stack.peak())
+stack.push(13)
+print(stack.peak())
+stack.push(14)
+print(stack.peak())
+stack.push(15)
+print(stack.peak())
+stack.push(16)
+print(stack.peak())
 print("список Stack пуст: ", stack.is_empty())
-print(stack.take_item())
-print(stack.take_item())
-print(stack.take_item())
-print(stack.take_item())
-print(stack.take_item())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
 print("список Stack пуст: ", stack.is_empty())
 
 
@@ -49,37 +49,65 @@ class Queue():
     def is_empty(self):
         return self.items ==[]
 
-    def queue_add(self, item):
+    def enqueue(self, item):
         return self.items.insert(0, item)
 
-    def queue_goes(self):
+    def dequeue(self):
         return self.items.pop()
 
-    def upper_elmt(self):
-        return self.items[-1]
+    def size(self):
+        return len(self.items)
 
 
 queue = Queue()
 
 print("список Queue пуст: ", queue.is_empty())
-queue.queue_add(12)
-queue.queue_add(13)
-queue.queue_add(14)
-queue.queue_add(15)
-queue.queue_add(16)
-print(queue.upper_elmt())
+queue.enqueue(12)
+queue.enqueue(13)
+queue.enqueue(14)
+queue.enqueue(15)
+queue.enqueue(16)
+print(queue.size())
 print("список Queue пуст: ", stack.is_empty())
-print(queue.queue_goes())
-print(queue.queue_goes())
-print(queue.queue_goes())
-print(queue.queue_goes())
-print(queue.queue_goes())
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
 print("список Queue пуст: ", queue.is_empty())
 
 
 
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
 
 
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.value < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+root = Node(70)
+
+root = insert(root, 30)
+root = insert(root, 56)
+root = insert(root, 89)
+root = insert(root, 45)
+root = insert(root, 60)
+root = insert(root, 73)
+root = insert(root, 98)
+root = insert(root, 84)
+print(root.value)
+print(root.left)
+print(root.left.right.left.value)
 
 
 
