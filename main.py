f0d8f712657e43407ec6a467f4e28c55e9e09762
Skeyling1 <skeyling1@gmail.com>
2025-01-19 14:22:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,4 +112,25 @@ print(root.left.right.left.value)
 
 
 
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def print_graph(self):
+        for node in self.graph:
+            print(node,'->', ' -> '.join(map(str, self.graph[node])))
+
+graph = Graph()
+graph.add_edge(0, 1)
+graph.add_edge(0, 4)
+graph.add_edge(1, 2)
+graph.add_edge(1, 3)
+graph.add_edge(1, 4)
+graph.add_edge(2, 3)
+graph.add_edge(3, 4)
+graph.print_graph()
